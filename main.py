2fbfbad79123bb241895e4cc3a5f34066abe469a
sherkoo <stephenherko@gmail.com>
2019-07-27 19:51:31 -0500

    Check for multiple letters
    The found variable locates multiple occurrences of the correctly guessed 
    letter

--- main.py
+++ main.py
@@ -4,23 +4,45 @@
 
 word = "testerman"
 word_len = len(word)
+word_guess = ["_","_","_","_","_","_","_","_","_"]
 tries_max = word_len - (word_len / 2)
 tries = 0
 playing = True
 
+# Start the game
 print("Lets play Hangman!")
-print("_" * word_len)
-print("Tries left: " + str(tries_max - tries))
+print(word_guess)
+
+# split the word into individual chars
+def split(word):
+    return [char for char in word]
 
 # While user hasn't reached max tries
 while playing:
   guess = raw_input("Guess a letter: ")
 
   if guess in word:
-    print("Correct!")
+    # User guesses a correct letter
+    print("=============================")
+    print("Letter Found!")
+    found = [pos for pos, char in enumerate(word) if char == guess]
+    # insert letter X found times
+    for f in found:
+        print(f)
+        word_guess[f] = guess
+
+    print(word_guess)
+    print("Tries left: " + str(tries_max - tries))
+
+    # end game if all letters are guessed
+    if word_guess == split(word):
+        print("YOU WIN!")
+        playing = False
+
   else:
     print("Sorry not found")
     tries += 1
+    print("Tries left: " + str(tries_max - tries))
 
     # End game if max tries is reached
     if tries == tries_max:
